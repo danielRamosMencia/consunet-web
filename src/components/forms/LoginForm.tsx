65b55d486f45693a1f051c5d751b52a411fdcb35
danielRamosMencia <danieldermramos@gmail.com>
2024-12-22 17:39:33 -0600

    feat: Button component

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,10 +1,32 @@
 import Button from "@components/ui/button/Button";
 
 const LoginForm = () => {
+  const onSubmit = () => {
+    console.log("Form submitted");
+  };
+
+  const onCancel = () => {
+    console.log("Form canceled");
+  };
+
   return (
     <div>
-      <Button />
-      <Button />
+      <Button
+        key={"login-button-submit"}
+        onClick={onSubmit}
+        type="submit"
+        variant="secondary"
+      >
+        Ingresar
+      </Button>
+      <Button
+        key={"login-button-cancel"}
+        onClick={onCancel}
+        type="submit"
+        variant="cancel"
+      >
+        Cancelar
+      </Button>
     </div>
   );
 };
--- src/components/ui/button/Button.tsx
+++ src/components/ui/button/Button.tsx
@@ -1,5 +1,45 @@
-const Button = () => {
-  return <div>Button</div>;
+type ButtonVariant = "primary" | "secondary" | "danger" | "cancel";
+
+type ButtonProps = {
+  children: React.ReactNode;
+  onClick?: () => void;
+  disabled?: boolean;
+  type?: "button" | "submit" | "reset";
+  variant?: ButtonVariant;
+};
+
+const Button = ({
+  children,
+  onClick,
+  disabled = false,
+  type = "button",
+  variant = "primary",
+}: ButtonProps) => {
+  const defaultClasses =
+    "font-bold py-2 px-6 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2";
+
+  const variantClasses: Record<ButtonVariant, string> = {
+    primary: "bg-blue-500 hover:bg-blue-700 text-white focus:ring-blue-500",
+    secondary:
+      "bg-green-500 hover:bg-green-700 text-white focus:ring-green-500",
+    danger: "bg-red-500 hover:bg-red-700 text-white focus:ring-red-500",
+    cancel: "bg-gray-500 hover:bg-gray-700 text-white focus:ring-gray-500",
+  };
+
+  const classes = `${defaultClasses} ${variantClasses[variant]}`;
+
+  return (
+    <button
+      onClick={onClick}
+      disabled={disabled}
+      type={type}
+      className={`${classes} ${
+        disabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
+    >
+      {children}
+    </button>
+  );
 };
 
 export default Button;
